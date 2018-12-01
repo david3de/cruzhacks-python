f8966edeb06dc41d3953d99c20d966edbef2f60f
David Lang <david3de@gmail.com>
2018-12-01 01:40:44 -0800

    added a bunch of stuff

---
+++ list.py
@@ -0,0 +1,13 @@
+# List examples
+mylist = ['foo', 'bar', 'baz']  # Initialize list
+
+print(mylist)
+print(mylist[2])
+
+# An example of looping through a list
+for ele in mylist:              # iterate through list elements
+	print(ele)
+
+# An example of enumerating through a list
+for index, ele in enumerate(mylist):
+	print(index, ele)
--- print.py
+++ print.py
@@ -1,1 +1,2 @@
+# Basic print
 print("Hello world!")
