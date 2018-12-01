cd37f52eaf0cf58364f209dab68cb7afa1e02d33
jchong1 <jocchong@ucsc.edu>
2018-12-01 01:49:16 -0800

    yuh

--- list.py
+++ list.py
@@ -4,10 +4,15 @@ mylist = ['foo', 'bar', 'baz']  # Initialize list
 print(mylist)
 print(mylist[2])
 
+# for (int i = 0; i < mylist.length; i++)
+for i in range(len(mylist)):
+    print(mylist[i])
+
 # An example of looping through a list
-for ele in mylist:              # iterate through list elements
+for ele in mylist:
 	print(ele)
 
 # An example of enumerating through a list
 for index, ele in enumerate(mylist):
 	print(index, ele)
+
